173de8c4c39ac4ce4714fc87e14efce135cf3e36
nivso-22 <122080004+nivso-22@users.noreply.github.com>
2023-01-06 09:30:54 +0200

    initial build 0.0.2
    draw-graph gets the graphs now

--- main.py
+++ main.py
@@ -25,17 +25,23 @@ def change_value(*args):
     x_in_middle.grid(row=3, column=1)
     range_end.grid(row=3, column=2)
 
-    graph_button = Button(function_entry_frame, text="graph", command=draw_graph)
+    graph_button = Button(function_entry_frame, text="graph", command= lambda: draw_graph(a_x, b_y, c, a_x_quad, b_quad, c_quad))
     graph_button.grid(row=4)
 
     root.update()
 
+    a_x = Entry(function_entry_frame, width=3)
+    b_y = Entry(function_entry_frame, width=3)
+    c = Entry(function_entry_frame, width=3)
+
+    a_x_quad = Entry(function_entry_frame, width=3)
+    b_quad = Entry(function_entry_frame, width=3)
+    c_quad = Entry(function_entry_frame, width=3)
+
     if drop_value == 'linear equation':
         root.update()
         # create the equation variables
-        a_x = Entry(function_entry_frame, width=3)
-        b_y = Entry(function_entry_frame, width=3)
-        c = Entry(function_entry_frame, width=3)
+
 
         x_plus_text = Label(function_entry_frame, text="X + ")
         y_plus_text = Label(function_entry_frame, text="Y + ")
@@ -43,10 +49,13 @@ def change_value(*args):
 
         # draw the equation variables
         a_x.grid(row=1,column=0)
+        a_x.insert(0, "0")
         x_plus_text.grid(row=1,column=1)
         b_y.grid(row=1,column=2)
+        b_y.insert(0, "0")
         y_plus_text.grid(row=1,column=3)
         c.grid(row=1,column=4)
+        c.insert(0, "0")
         equals_0_text.grid(row=1, column=5)
 
         root.geometry("185x300")
@@ -55,9 +64,7 @@ def change_value(*args):
     if drop_value == 'quadratic equation':
         root.update()
 
-        a_x_quad = Entry(function_entry_frame, width=3)
-        b_quad = Entry(function_entry_frame, width=3)
-        c_quad = Entry(function_entry_frame, width=3)
+
 
         x_plus_text = Label(function_entry_frame, text="X² + ")
         y_plus_text = Label(function_entry_frame, text="X + ")
@@ -65,10 +72,13 @@ def change_value(*args):
 
         # draw the equation variables
         a_x_quad.grid(row=1, column=0)
+        a_x_quad.insert(0, "0")
         x_plus_text.grid(row=1, column=1)
         b_quad.grid(row=1, column=2)
+        b_quad.insert(0, "0")
         y_plus_text.grid(row=1, column=3)
         c_quad.grid(row=1, column=4)
+        c_quad.insert(0, "0")
         equals_0_text.grid(row=1, column=5)
 
         root.geometry("185x300")
@@ -77,9 +87,20 @@ def change_value(*args):
     root.update()
 
 
-def draw_graph():
+def draw_graph(a_x, b_y, c, a_x_quad, b_quad, c_quad):
+    global selected_value
     print("imagine i made a graph here")
+    if selected_value.get() == 'linear equation':
+        a_x_draw = int(a_x.get())
+        b_y_draw = int(b_y.get())
+        c_draw = int(c.get())
+        print(a_x_draw, "x +", b_y_draw, "y +", c_draw, "= 0 ")
 
+    if selected_value.get() == 'quadratic equation':
+        a_x_quad_draw = int(a_x_quad.get())
+        b_quad_draw = int(b_quad.get())
+        c_quad_draw = int(c_quad.get())
+        print(a_x_quad_draw, "x² +", b_quad_draw, "x +", c_quad_draw, "= 0 ")
 
 
 
