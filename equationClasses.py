ceace557d5f9b27c12845670fd4c3e6d1aabf7ae
nivso-22 <122080004+nivso-22@users.noreply.github.com>
2023-01-17 19:51:19 +0200

    new equation classes

--- equationClasses.py
+++ equationClasses.py
@@ -1,29 +1,70 @@
+import math
+
+
 class LinearEquation:
     a_x = 0
     b_y = 0
     c = 0
+    color = ""
+    range_start = 0
+    range_end = 0
 
-    def __init__(self, a_x, b_y, c):
-        self.a_x = a_x
-        self.b_y = b_y
-        self.c = c
+    def __init__(self, a_x, b_y, c, color, r_s, r_e):
+        self.a_x = int(a_x)
+        self.b_y = int(b_y)
+        self.c = int(c)
+        self.color = color
+        self.range_start = int(r_s)
+        self.range_end = int(r_e)
 
     def __str__(self):
         return str(self.a_x) + "X + " + str(self.b_y) + "Y + " + str(self.c) + " = 0"
+
+    def get_color(self):
+        return self.color
+
+    def get_plot(self):
+        x_list = []
+        y_list = []
+        for x in range(self.range_start, self.range_end):
+            x_list.append(x)
+            y_list.append((-self.a_x/self.b_y)*x + -self.c/self.b_y)
+        return x_list, y_list
 
 
 class QuadraticEquation:
     a_x_quad = 0
     b_x = 0
     c = 0
+    color = ""
+    range_start = 0
+    range_end = 0
 
-    def __init__(self, a_x_quad, b_x, c):
-        self.a_x_quad = a_x_quad
-        self.b_y = b_x
-        self.c = c
+    def __init__(self, a_x_quad, b_x, c, color, r_s, r_e):
+        self.a_x_quad = int(a_x_quad)
+        self.b_y = int(b_x)
+        self.c = int(c)
+        self.color = color
+        self.range_start = int(r_s)
+        self.range_end = int(r_e)
 
     def __str__(self):
         return str(self.a_x_quad) + "X² + " + str(self.b_x) + "x + " + str(self.c) + " = 0"
+
+    def get_color(self):
+        return self.color
+
+    def get_plot(self):
+        x_list = []
+        y_list = []
+        for x in range(self.range_start, int(self.range_end)+1):
+            x_list.append(x)
+            y_list.append(self.a_x_quad * x ** 2 + self.b_x * x + self.c)
+
+        return x_list, y_list
+
+    def get_extrema(self):
+        return (-self.b_x) / 2*self.a_x_quad, self.c - (self.b_x**2/4*self.a_x_quad)
 
 
 class CubicEquation:
@@ -31,67 +72,175 @@ class CubicEquation:
     b_x = 0
     c_x = 0
     d = 0
+    color = ""
+    range_start = 0
+    range_end = 0
 
-    def __init__(self, a_x_cube, b_x_squared, c_x, d):
-        self.a_x_cube = a_x_cube
-        self.b_y = b_x_squared
-        self.c_x = c_x
-        self.d = d
+    def __init__(self, a_x_cube, b_x_squared, c_x, d, color, r_s, r_e):
+        self.a_x_cube = int(a_x_cube)
+        self.b_x = int(b_x_squared)
+        self.c_x = int(c_x)
+        self.d = int(d)
+        self.color = color
+        self.range_start = int(r_s)
+        self.range_end = int(r_e)
+        self.x_extremum_1 = 0
+        self.x_extremum_2 = 0
+        self.y_extremum_1 = 0
+        self.y_extremum_2 = 0
 
     def __str__(self):
         return str(self.a_x_cube) + "X³ + " + str(self.b_x) + "x² + " + str(self.c_x) + "X + " +str(self.d) + " = 0"
 
+    def get_color(self):
+        return self.color
+
+    def get_plot(self):
+        x_list = []
+        y_list = []
+        for x in range(self.range_start, self.range_end + 1):
+            x_list.append(x)
+            y_list.append((self.a_x_cube * x ** 3 + self.b_x * x**2 + self.c_x * x + self.d))
+
+        return x_list, y_list
+
+    def get_extrema(self):
+        self.x_extremum_1 = (-self.b_x - math.sqrt(self.b_x ** 2 - (3 * self.a_x_cube * self.c_x))) / 3 * self.a_x_cube
+        self.x_extremum_2 = (-self.b_x + math.sqrt(self.b_x ** 2 - (3 * self.a_x_cube * self.c_x))) / 3 * self.a_x_cube
+
+        self.y_extremum_1 = self.x_extremum_1 ** 3 * self.a_x_cube + self.x_extremum_1 ** 2 * self.b_x + self.x_extremum_1 * self.c_x + self.d
+        self.y_extremum_2 = self.x_extremum_2 ** 3 * self.a_x_cube + self.x_extremum_2 ** 2 * self.b_x + self.x_extremum_2 * self.c_x + self.d
+
 
 class LogarithmicEquationAX:
     a = 0
+    color = ""
+    range_start = 0
+    range_end = 0
 
-    def __init__(self, a):
-        self.a = a
+    def __init__(self, a, color, r_s, r_e):
+        self.a = int(a)
+        self.color = color
+        self.range_start = int(r_s)
+        self.range_end = int(r_e)
 
     def __str__(self):
         return "log " + str(self.a) + " (X)"
 
+    def get_color(self):
+        return self.color
+
+    def get_plot(self):
+        x_list = []
+        y_list = []
+        for x in range(self.range_start, self.range_end + 1):
+            x_list.append(x)
+            y_list.append(math.log(x, self.a))
+
+        return x_list, y_list
+
 
 class LogarithmicEquationXA:
     a = 0
+    color = ""
+    range_start = 0
+    range_end = 0
 
-    def __init__(self, a):
-        self.a = a
+    def __init__(self, a, color, r_s, r_e):
+        self.a = int(a)
+        self.color = color
+        self.range_start = int(r_s)
+        self.range_end = int(r_e)
 
     def __str__(self):
-        return "log " + "X (" + str(self.a) + ")"
+        return "log X (" + str(self.a) + ")"
+
+    def get_color(self):
+        return self.color
+
+    def get_plot(self):
+        x_list = []
+        y_list = []
+        for x in range(self.range_start, self.range_end + 1):
+            x_list.append(x)
+            y_list.append(math.log(self.a, x))
+
+        return x_list, y_list
 
 
 class ExponentialEquationAX:
     a = 0
+    color = ""
+    range_start = 0
+    range_end = 0
 
-    def __init__(self, a):
-        self.a = a
+    def __init__(self, a, color, r_s, r_e):
+        self.a = int(a)
+        self.color = color
+        self.range_start = int(r_s)
+        self.range_end = int(r_e)
 
     def __str__(self):
         return str(self.a) + "^X"
 
+    def get_color(self):
+        return self.color
+
+    def get_plot(self):
+        x_list = []
+        y_list = []
+        for x in range(self.range_start, self.range_end + 1):
+            x_list.append(x)
+            y_list.append(self.a ** x)
+
+        return x_list, y_list
+
 
 class ExponentialEquationXA:
     a = 0
+    color = ""
+    range_start = 0
+    range_end = 0
 
-    def __init__(self, a):
-        self.a = a
+    def __init__(self, a, color, r_s, r_e):
+        self.a = int(a)
+        self.color = color
+        self.range_start = int(r_s)
+        self.range_end = int(r_e)
 
     def __str__(self):
-        return "X^" + str(self.a)
+        return "X^ " + str(self.a)
+
+    def get_color(self):
+        return self.color
+
+    def get_plot(self):
+        x_list = []
+        y_list = []
+        for x in range(self.range_start, self.range_end + 1):
+            x_list.append(x)
+            y_list.append(x**self.a)
+
+        return x_list, y_list
 
 
 class CircleEquation:
     a = 0
     b = 2
     c = 2
+    color = ""
 
-    def __init__(self, a, b, c):
+    def __init__(self, a, b, c, color):
         self.a = a
         self.b = b
         self.c = c
+        self.color = color
 
     def __str__(self):
         return "(x-" + str(self.a) + ")² + (y-" + str(self.b) + ")² = " + str(self.c)
+
+    def get_color(self):
+        return self.color
+
+
 
