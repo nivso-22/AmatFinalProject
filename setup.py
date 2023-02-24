dfd1daa394caeb34dd6f3542e5c65845eb05d524
nivso-22 <122080004+nivso-22@users.noreply.github.com>
2023-02-24 10:34:46 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='desNOT',
+    version='1.0',
+    description='My bad graphing program',
+    author='Niv Sorek',
+    packages=['desNOT'],
+    install_requires=[
+        'customtkinter',
+        'matplotlib',
+        'tkinter',
+    ],
+)
